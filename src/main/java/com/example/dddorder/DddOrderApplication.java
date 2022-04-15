e35c3e1054d036e17ac0e9ad7fc1152e8e391f1a
aeby <withwooyong@gmail.com>
2022-04-15 09:40:23 +0900

    MSA를 위해 DDD 적용할 order 프로젝트

---
+++ src/main/java/com/example/dddorder/DddOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.dddorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dddorder/DddOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dddorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
